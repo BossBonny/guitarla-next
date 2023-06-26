55ca672450d2c6b2dc19ccaab3518d74fe92fef4
John <johnari151298@gmail.com>
2023-06-26 18:42:38 +0200

    Segundo Commit

--- components/post.js
+++ components/post.js
@@ -15,7 +15,7 @@ export default function Post({post}) {
                 <p>{contenido}</p>
                 <Link href={`/blog/${url}`} legacyBehavior>
                     <a>
-                        Leer Post
+                        Leer Post, ahh prrro esta es la prueba
                     </a>
                 </Link>
             </div>
